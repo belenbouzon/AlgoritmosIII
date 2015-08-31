7fa7c723e9962ade4afe7b7e507593860f9d8cd9
Paula Jimenez <puly05@gmail.com>
2015-08-31 01:28:33 -0300

    modifico el genTest.sh

--- TP1/src/ej2/test/genTest.sh
+++ TP1/src/ej2/test/genTest.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 file=$1
-cantLineas=$2
-maxN=$3
+maxN=$2
+espaciado=$3
 touch $file
 
-for ((m=1; m<$cantLineas; m++))
+for ((m=1; m < $maxN; m+=espaciado))
 do
 	line=""
-	n=$[ 1 + $[ RANDOM % $maxN ]]
-	for ((i=1; i<=$n; i++))
+	for ((i=0; i<$m; i++))
 	do
 		num=$[ 1 + $[ RANDOM % 10000 ]]
 		line="$line $num "
