b3e8f6bb75a359c9e2b4f2270363fa847c187106
MaximilianoRey <rey.maximiliano@gmail.com>
2015-09-01 00:01:26 -0300

    ej 3 anda (me olvide del Main)

--- TP1/src/ej3/ej3/src/Main.java
+++ TP1/src/ej3/ej3/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	public static void main(String[] args) throws Exception{
 		Solucion sol = new Solucion();
-		//Lector lec = new Lector(args[0]);
-		Lector lec = new Lector("../bin/Tp1Ej3_3.in");
+		Lector lec = new Lector(args[0]);
+		//Lector lec = new Lector("../bin/Tp1Ej3_3.in");
 		sol.generar_solucion(lec.leer_palabra());
 	}
 }
