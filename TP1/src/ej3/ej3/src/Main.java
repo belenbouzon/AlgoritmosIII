694968a0cdb96dc4a8e12a0754058890744007bb
Pablo Montepagano <pablo@montepagano.com.ar>
2015-09-02 14:17:59 -0300

    Mini avance informe ej3

--- TP1/src/ej3/ej3/src/Main.java
+++ TP1/src/ej3/ej3/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		Solucion sol = new Solucion();
 		Lector lec = new Lector(args[0]);
-		//Lector lec = new Lector("../bin/Tp1Ej3_3.in");
 		sol.generar_solucion(lec.leer_palabra());
 	}
 }
