713e03785f89ea92cd0d75db1800663746a9fb13
Pau <pau@pau-linux.(none)>
2015-09-01 21:12:24 -0300

    cambios para testear performance desde bash

--- TP1/src/ej2/src/Main.java
+++ TP1/src/ej2/src/Main.java
@@ -7,9 +7,6 @@ public class Main {
 		int mediana;
 		int tam;
 		Heap heap = new Heap();
-		// Si no tiene el flag --tiempos, se ejecuta solo una vez.
-		int cantEjec = 1;
-		int j;
         long time0;
         long time1;
         Boolean mideTiempos = false;
@@ -19,7 +16,6 @@ public class Main {
         if (args.length > 1 && args[1].equalsIgnoreCase("--tiempos"))
         {
             mideTiempos = true;
-            cantEjec = Integer.parseInt(args[2]);
         }
 
 		Lector lect = new Lector(args[0]);
@@ -33,49 +29,39 @@ public class Main {
 			}
 			mediana = 0;
 			tam = conj.length;
-			//Para correr varias veces el algoritmo y testear performance.
-			j = 0;
-			while(j < cantEjec){
-				time0 = System.nanoTime();
+			time0 = System.nanoTime();
 
-				for(int i = 0; i < tam; i++){
-					val = conj[i];
-					if(val >= mediana){
-						heap.insertMinHeap(val);
-					}else{
-						heap.insertMaxHeap(val);
-					}
-					
-					heap.balancearHeaps();
-					
-					mediana = heap.calcularMediana();
-					//imprimir solucion
-					if(!mideTiempos){
-						esc.EscribirInt(mediana);
-					}
+			for(int i = 0; i < tam; i++){
+				val = conj[i];
+				if(val >= mediana){
+					heap.insertMinHeap(val);
+				}else{
+					heap.insertMaxHeap(val);
 				}
+				
+				heap.balancearHeaps();
+				
+				mediana = heap.calcularMediana();
+				//imprimir solucion
+				esc.EscribirInt(mediana);
+			}
 
-				if(mideTiempos){
-					time1 = System.nanoTime();
-					//separo las instancias del problema.
-					if(j == 0){
-						esc.EscribirInt(j);
-						esc.NuevaLinea();
-					}
-					esc.EscribirLong(time1-time0);
-				}
-				esc.NuevaLinea();
-				heap.ClearHeap();
-				j++;
-	        }
+			if(mideTiempos){
+				time1 = System.nanoTime();
+				time1 = time1-time0;
+				System.out.printf("%d-%s\n", tam, Long.toString(time1));
+			}
+			esc.NuevaLinea();
+			heap.ClearHeap();
 		}
         esc.Fin();
 	}
 	
-	private static void MostrarIndicaciones(int length){
+	private static void MostrarIndicaciones(int length) throws Exception{
 		if (length < 1){
-		    System.out.printf("Debe pasar el nombre del archivo de input como parametro. Ademas puede pasar el flag --tiempos despues del nombre de archivo, seguido de un numero entero, para correr las mediciones de tiempos esa cantidad de veces\n");
-		    System.out.printf("USO: java Main INPUT [--tiempos N]\n");
+		    System.out.printf("Debe pasar el nombre del archivo de input como parametro. Ademas puede pasar el flag --tiempos despues del nombre de archivo para obtener las mediciones de tiempos\n");
+		    System.out.printf("USO: java Main INPUT [--tiempos]\n");
+			System.exit(0);
 		}
 	}
 }
---
+++ TP1/src/ej2/test/testPerformance.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cantFiles=$1
+touch resultados.out
+echo "n prom" > resultados.out
+
+for (( file = 1; file <= $cantFiles; file++ )) 
+do
+	max=0
+	min=99999999
+	promedio=0
+
+
+	for i in {1..100}
+	do
+	    clocks=$(java -cp ../bin/ Main $file --tiempos)
+	    n=$(echo $clocks | cut -d "-" -f 1)
+	    clocks=$(echo $clocks | cut -d "-" -f 2)
+
+	    let promedio+=$clocks
+	    #echo $promedio
+
+	    if [[ "$clocks" -gt "$max" ]]; then
+	    	max=$clocks
+	    fi
+
+	    if [[ "$clocks" -lt "$min" ]]; then
+	    	min=$clocks
+	    fi
+	    
+	done
+
+
+	promedio=$(($promedio-$min-$max))
+	promedio=$(($promedio/98))
+
+	echo $n $promedio >> resultados.out
+
+done
+
+exit 0
