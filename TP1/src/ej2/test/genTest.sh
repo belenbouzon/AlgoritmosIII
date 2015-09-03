b0923fc5e87db6628f30ce26f6378a457bf78619
Paula Jimenez <puly05@gmail.com>
2015-09-03 17:56:45 -0300

    tests de performance

--- TP1/src/ej2/test/genTest.sh
+++ TP1/src/ej2/test/genTest.sh
@@ -8,14 +8,14 @@ let maxN+=1
 
 for (( m = 1; m <= $maxN; m+=$espaciado ))
 do
-	touch $count
+	touch "$count.t"
 	line=""
 	for (( i = 0; i < $m; i++ ))
 	do
 		num=$[ 1 + $[ RANDOM % 10000 ]]
 		line="$line $num"
 	done
-	echo $line >> $count
+	echo $line >> "$count.t"
 	let count+=1
 done
 
