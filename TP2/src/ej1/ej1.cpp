ea2f75e56f763655a8ab4b4d36e3df959fe69d7e
Pablo Montepagano <pablo@montepagano.com.ar>
2015-09-22 01:27:22 -0300

    Agrego codigo para medir tiempos y modifico el parseo para no usar regex

--- TP2/src/ej1/ej1.cpp
+++ TP2/src/ej1/ej1.cpp
@@ -5,49 +5,60 @@
 #include <list>
 #include <iostream>
 #include <regex>
+#include <chrono>
 
 using namespace std;
 
 int main( int argc, char* argv[]){
     bool debug = false;
-    /*
     bool tiempos = false;
     int corridas = 1;                      // cantidad de corridas a hacer para medir tiempos
+    string nombreInput = "";
+    if (argc < 2){
+        cout << "Uso:\n./ej1 [--tiempos] [--debug] infile.in\n";
+    }
     for (int i=1; i<argc; ++i){
         string p = argv[i];
         if (p == "--debug"){
             debug = true;
-        }
-        if (p == "--topdown"){
-            topdown = true;
-        }
-        if (p == "--tiempos"){
+        } else if (p == "--tiempos"){
             tiempos = true;
             corridas = 100;
+        } else{
+            nombreInput = p; 
         }
     }
-    */
 
+    int p = 0;  // quiero saber la cantidad total de portales para el análisis de tiempos
 
+    std::ifstream infile(nombreInput);
+    std::ofstream outfile(nombreInput+".out");
 
-    std::ifstream infile("input.txt");
+    if (tiempos){
+        cout << "n,p,t" << endl;
+    }
     
     std::string line;
     while (std::getline(infile, line)){ // aca levanto la primera linea del problema (n)
         int n = stoi(line);
         vector< int > resultados;
         vector< list < int > > portales;
-        portales.resize(n);
-        resultados.resize(n);
+        portales.resize(n);     // O(n)
+        resultados.resize(n);   // O(n)
         for (int i=0; i<n-1; ++i){      // inicializo portales (input) y resultados (tabla resultados parciales)
             std::list<int> l;
             portales[i] = l;
             resultados[i] = -1;
-        }
+        }   // O(n)
         
         string lineaPortales;
         std::getline(infile, lineaPortales);
 
+        /*
+         * Este parseo con regex lo dejo comentado
+         * porque no encuentro en la documentación
+         * la complejidad. Lo reemplacé con el código
+         * de más abajo (que es menos claro)
         std::regex reg1("\\s*(\\d+)\\s+(\\d+)\\s*[;\\n$]*");
         sregex_iterator it(lineaPortales.begin(), lineaPortales.end(), reg1);
         sregex_iterator it_end;
@@ -56,8 +67,44 @@ int main( int argc, char* argv[]){
             int sale = stoi(res[1]);
             int llega = stoi(res[2]);
             portales[sale].push_back(llega);
+            p++;
             ++it;
+        }   // O(P)
+        */
+
+
+        // parseo portales
+        std::string delimiter = "; ";
+        size_t pos = 0;
+        size_t espacio = 0;
+        std::string token;
+        while ((pos = lineaPortales.find(delimiter)) != std::string::npos) {
+            token = lineaPortales.substr(0, pos);
+            // ahora falta separar el primer portal del segundo
+            if ((espacio = token.find(' ')) != std::string::npos){
+                int sale = stoi(token.substr(0,espacio));
+                int llega = stoi(token.substr(espacio));
+                portales[sale].push_back(llega);
+                p++;
+            } else{
+                cout << "ERROR DE PARSEO!!!" << endl;
+                exit(1);
+            }
+
+            lineaPortales.erase(0, pos + delimiter.length());
         }
+        // queda el último
+        if ((espacio = lineaPortales.find(' ')) != std::string::npos){
+            int sale = stoi(lineaPortales.substr(0,espacio));
+            int llega = stoi(lineaPortales.substr(espacio));
+            portales[sale].push_back(llega);
+        } else{
+            cout << "ERROR DE PARSEO!!!" << endl;
+            exit(1);
+        }
+
+        // todo este parseo de los portales es O(P)
+
     
         // ya quedaron los datos de entrada del problema cargados.
 
@@ -71,37 +118,41 @@ int main( int argc, char* argv[]){
                 cout << endl;
             }
         }
-
-        // resolucion:
-        int i = n-1;
-        while (i>=0){
-            int max = -1;
-            for (std::list<int>::iterator it=portales[i].begin(); it != portales[i].end(); ++it){
-                int costo = -1;
-                if (*it == n){
-                    // el portal va directo a N
-                    costo = 1;
-                } else if (resultados[*it] == -1){
-                    // no se puede llegar a N por este portal
-                    costo = -1;
-                } else{
-                    costo = resultados[*it] + 1;
+        int ejecucion = 0;
+        while (ejecucion < corridas){
+            // resolucion:
+            std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
+            int i = n-1;
+            while (i>=0){
+                int max = -1;
+                for (std::list<int>::iterator it=portales[i].begin(); it != portales[i].end(); ++it){
+                    int costo = -1;
+                    if (*it == n){
+                        // el portal va directo a N
+                        costo = 1;
+                    } else if (resultados[*it] == -1){
+                        // no se puede llegar a N por este portal
+                        costo = -1;
+                    } else{
+                        costo = resultados[*it] + 1;
+                    }
+                    if (costo > max){
+                        max = costo;
+                    }
                 }
-                if (costo > max){
-                    max = costo;
-                }
+                resultados[i] = max;
+                i--;
             }
-            resultados[i] = max;
-            i--;
-        }
-
-        cout << resultados[0] << endl;
-        
-    }
-
-
-    
-
+            if (!tiempos){
+                outfile << resultados[0] << endl;
+            }else{
+                std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t1 - t0 ).count();
+                cout << n << "," << p << "," << duration << endl;
+            }
+            ejecucion++;
+        } // terminan todas las corridas
+    } // termino problema
 
 	return 0;
 }
