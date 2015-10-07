9c3df89b91f21369e8c2a2576869b4d4b4061bec
MaximilianoRey <rey.maximiliano@gmail.com>
2015-10-06 21:49:30 -0300

    borre archivo

--- TP2/src/ej2/src/ComparadorNodo.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorNodo implements Comparator<Nodo>{
-	public int compare(Nodo n1, Nodo n2){
-		return n1.identificacion-n2.identificacion;
-	}
-}
