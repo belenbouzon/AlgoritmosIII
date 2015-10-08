7f3105386ee753477b21df1a9cb637b09ea85067
Paula Jimenez <puly05@gmail.com>
2015-10-08 13:01:37 -0300

    ej3: si ya puse n-1 aristas solo sumo las distancias

--- TP2/src/ej3/src/Grafo.java
+++ TP2/src/ej3/src/Grafo.java
@@ -6,10 +6,12 @@ public class Grafo {
 
    private List<Pasillo> _pasillos;
    private UnionFind _sets;
+   private int _nodos;
 
    public Grafo( int n, List<Pasillo> ps ) {
 	   OrdenarDesc(ps);
       _pasillos = ps;
+      _nodos = n;
       _sets = new UnionFind(n);
    }
    
@@ -20,17 +22,25 @@ public class Grafo {
 
    public final int kruskal() {
 	   int suma = 0;
+	   int aristasAG = 0;
 	   Pasillo p;
 	   ListIterator<Pasillo> it = _pasillos.listIterator();
 	   while(it.hasNext()){
 		   p = it.next();
-		   if(_sets.isSameSet(p.getExtremo1(), p.getExtremo2())){
-			   //si estan en el mismo set
-			   //tengo que sumar la long del pasillo a clausurar.
-			   suma += p.getLongitud();
-		   }else{
-			   _sets.unionSet(p.getExtremo1(), p.getExtremo2());
-		   }
+		   if(aristasAG < _nodos - 1){
+			   if(_sets.isSameSet(p.getExtremo1(), p.getExtremo2())){
+				   //si estan en el mismo set
+				   //tengo que sumar la long del pasillo a clausurar.
+				   suma += p.getLongitud();
+			   }else{
+				   _sets.unionSet(p.getExtremo1(), p.getExtremo2());
+				   //sumo una arista al AG.
+				   aristasAG++;
+			   }
+			}else{
+				// si ya puse n-1 aristas solo sumo las distancias.
+				suma += p.getLongitud();
+			}
 	   }
 	   return suma;
    }
