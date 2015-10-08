3231201bbe694c5be7391f4cc87f6f759b960c33
Pablo Montepagano <pablo@montepagano.com.ar>
2015-10-08 00:54:13 -0300

    Para compilar esta mierda

---
+++ TP2/src/ej2/src/compilar.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+javac BoundedIntegerMap.java DobleTupla.java GeneradorDeEntradasExtensible.java GeneradorRandomDeEntradas.java Lector.java Main.java Nodo.java Solucion.java Tester.java Tester2.java Tester3.java Tester_correctitud.java -classpath junit__V4.4__I2007.07.19.jar
