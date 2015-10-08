a4fe5f608527bebdff2225e346c3beeee5f7329d
Paula Jimenez <puly05@gmail.com>
2015-10-08 16:15:49 -0300

    ej3: arreglo error en generador de tests

--- TP2/src/ej3/test/genTest.sh
+++ TP2/src/ej3/test/genTest.sh
@@ -6,13 +6,13 @@ N=$1
 muestras=$2
 count=1
 # cant de aristas del grafo completo de N nodos.
-let maxM=$N*($N-1)/2
+maxM=$(($N*($N-1)/2))
 let espaciado=($maxM-$N+1)/$muestras
 # m = cantidad de aristas del grafo
 for (( m = $N-1; m <= $maxM; m+=$espaciado ))
 do
-	touch "$count.t"
-	python random_connected_graph.py $N -e $m > "$count.t"
+	touch "$count-$N.t"
+	python random_connected_graph.py $N -e $m > "$count-$N.t"
 	let count+=1
 done
 
