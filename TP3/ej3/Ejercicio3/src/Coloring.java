eedf40b1f8aa898abb674af2a08864c32b1e87a1
belenbouzon <belenbouzon@gmail.com>
2015-11-07 22:07:14 -0200

    Agrego cosas ejercicio 3

--- TP3/ej3/Ejercicio3/src/Coloring.java
+++ TP3/ej3/Ejercicio3/src/Coloring.java
@@ -1,4 +1,6 @@
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Coloring 
 {
@@ -39,7 +41,35 @@ public class Coloring
 
 	private static int CalcularColorMenosImportante(Nodo nodoActual, Grafo grafo) 
 	{//modificar de acuerdo a las heuristicas. Pasar a abstracto.
+		Double pesoColor = 0.0;
+		int colorAEliminar = -1;
+		for (int color : nodoActual.getColoresRestantes())
+		{
+			Double peso = CalcularPeso(color, grafo.getVecinosDe(nodoActual));
+			if (peso > pesoColor)
+			{
+				pesoColor = peso;
+				colorAEliminar = color;
+			}	
+		}	
 		return nodoActual.getColoresRestantes().getFirst(); //BORRAR esta implementacion. Ojo que ese metodo saca el primer elemento.
+	}
+
+	private static Double CalcularPeso(int color, List<Nodo> vecinos) 
+	{
+		LinkedList<Double> pesos = new LinkedList<Double>();
+		for (Nodo nodo : vecinos)
+		{
+			if (nodo.getColoresRestantes().contains(color))
+					pesos.add((1.0/nodo.getColoresRestantes().size()));
+					
+		}
+		Collections.sort(pesos);
+		Double pesoTotal = 0.0;
+		for (int k = 0; k < pesos.size(); k++)
+			pesoTotal += pesos.get(k)/(k+2);
+		
+		return pesoTotal;
 	}
 
 	private static Nodo PicFirstNode(Grafo grafo) 
