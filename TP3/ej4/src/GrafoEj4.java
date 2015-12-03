20f296832faa0523bf596ecbd3256de2f71340cf
Pablo Montepagano <pablo@montepagano.com.ar>
2015-12-03 05:11:41 -0300

    Hecho el conversor de grafoej3 a grafoej4

--- TP3/ej4/src/GrafoEj4.java
+++ TP3/ej4/src/GrafoEj4.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class GrafoEj4 {
@@ -9,8 +11,54 @@ public class GrafoEj4 {
 	public int cantConflictos(){
 		return this._conflictos.size();
 	}
+	
+	public NodoConVecinos getNodo(int id){
+		// obtiene un nodo del grafo a partir de su ID
+		return this._nodos.get(id);
+	}
+	
 	public GrafoEj4(Grafo otro){
 		// Convierte un grafo del ej3 en un grafo del ej4
+		/*
+			public int cantidadDeNodos;
+			private ArrayList<Nodo> nodos;
+			private int cantidadDeAristas;
+			private int cantidadDeColores;
+			ArrayList<List<Nodo>> listaDeAdyacencia;
+		 */
+		this._cantidadDeColores = otro.getCantidadDeColores();
+		
+		// agrego todos los nodos (sin aristas)
+		for (Iterator<Nodo> n = otro.getNodos().iterator() ; n.hasNext(); ){
+			// convierto el Nodo a NodoConVecinos
+			Nodo original = n.next();
+			NodoConVecinos convertido = new NodoConVecinos(original);
+			
+			// Agrego el Nodo al grafo
+			this._nodos.add(convertido);
+		}
+		
+		
+		// Armo la lista de adyacencia del nodo. Tengo que ciclar de nuevo
+		// porque primero necesito que esten todos los nodos creados.
+		for (Iterator<Nodo> n = otro.getNodos().iterator() ; n.hasNext(); ){
+			// convierto el Nodo a NodoConVecinos
+			Nodo original = n.next();
+			NodoConVecinos convertido = this.getNodo(original.getId());
+			
+			for (Iterator <Nodo> v = otro.getVecinosDe(original).iterator(); v.hasNext(); ){
+				Nodo vecinoOriginal = v.next();
+				NodoConVecinos vecinoConvertido = this.getNodo(vecinoOriginal.getId());
+				convertido.agregarVecinos(vecinoConvertido);
+				// Chequeo si hay conflicto.
+				if (convertido.getColor() == vecinoConvertido.getColor()){
+					// Hay conflicto!
+					Arista c = new Arista(convertido,vecinoConvertido);
+					this._conflictos.add(c);
+					// Vamos a estar agregando cada conflicto dos veces, pero como el equals de Arista chequea la igualdad, no se agrega dos veces al Set.
+				}
+			}
+		}
 		
 	}
 	
