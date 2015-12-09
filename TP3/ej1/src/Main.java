7fb5d96623c4db19f80e01d3f8202a5e875c6551
Maximiliano Rey <mxrey@ws6.labo6>
2015-12-09 18:12:35 -0300

    medidor de tiempos en Main de ej1

--- TP3/ej1/src/Main.java
+++ TP3/ej1/src/Main.java
@@ -4,14 +4,27 @@ import java.io.IOException;
 public class Main {
 	public static void main(String [] entrada) throws IOException{
 		if(entrada.length==0){
-			System.err.print("error, ingrese nombre de archivo");
+			System.err.print("Integrese: <nombre_de_archivo>; si quiere una medición de tiempos ingrese --imprimirTiempos como segundo parámetro\n");
 			return;
 		}
-		Lector lec = new Lector(entrada[0]);
-		lec.inicializar_lector();
-		Calculador_de_Coloracion_Ej1 res = new Calculador_de_Coloracion_Ej1(lec.cantidad_nodos(),lec.nodos_del_grafo());
-		String solucion = res.obtener_resolucion();
-		System.out.print(solucion);
-		System.out.print("\n");
+		if(entrada.length==1 || !entrada[1].equal("--imprimirTiempos")){
+			Lector lec = new Lector(entrada[0]);
+			lec.inicializar_lector();
+			Calculador_de_Coloracion_Ej1 res = new Calculador_de_Coloracion_Ej1(lec.cantidad_nodos(),lec.nodos_del_grafo());
+			String solucion = res.obtener_resolucion();
+			System.out.print(solucion);
+			System.out.print("\n");
+		}else{
+			Lector lec = Lector.crear_lector_cargado(entrada[0]);
+			long inicio = System.nanoTime();
+			lec.inicializar_lector();
+			Calculador_de_Coloracion_Ej1 res = new Calculador_de_Coloracion_Ej1(lec.cantidad_nodos(),lec.nodos_del_grafo());
+			String solucion = res.obtener_resolucion();
+			long fin = System.nanoTime();
+			//System.out.print(solucion);
+			//System.out.print("\n");
+			System.out.print(Integer.toString(fin-inicio));
+			System.out.print("\n");
+		}
 	}
 }
